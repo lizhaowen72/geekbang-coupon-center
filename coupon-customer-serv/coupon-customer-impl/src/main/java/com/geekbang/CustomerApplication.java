1461af80a03319843c6ae85c4abe2c420839502f
lizhaowen <lzw1987913QQ>
2022-12-04 12:46:14 +0800

    nacos 服务

--- coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/CustomerApplication.java
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/CustomerApplication.java
@@ -3,19 +3,20 @@ package com.geekbang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"com.geekbang"})
-@EntityScan(basePackages = {"com.geekbang"})
-public class Application {
+@ComponentScan(basePackages = {"com.geekbang"})
+@EnableDiscoveryClient
+public class CustomerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(CustomerApplication.class, args);
     }
 
 }
